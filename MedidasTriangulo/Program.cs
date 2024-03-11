ebfd820c0442f0f92e6667c7e37d112daee90dbb
Caroline Ramos <carolbeatrice@gmail.com>
2024-03-10 23:30:18 -0300

    iniciando projeto

---
+++ MedidasTriangulo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MedidasTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fazer um programa para ler os valores da largura e altura de um retângulo. Em seguida, mostrar na tela o valor de sua área, perímetro e diagonal. A Classe para esta atividade deve conter os atributos Largura e Altura e os Métodos CalcularArea() : double, CalcularPerimetro:double e CalcularDiagonal(): double
+
+
+        }
+    }
+}
